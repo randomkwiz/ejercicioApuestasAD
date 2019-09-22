69e53ac79da54342650a17eb8722d068b8b33c2c
AV <angyvazquezdel97@gmail.com>
2019-09-22 18:55:46 +0200

    Correcciones en el Trigger de comprobar si el cupo de apuestas está abierto. + Trigger no apostar más de lo que se tiene

--- Triggers_ApuestasDeportivas.sql
+++ Triggers_ApuestasDeportivas.sql
@@ -16,11 +16,11 @@ ON APUESTATIPO1
 AFTER INSERT, UPDATE AS
 BEGIN
 	IF EXISTS
-		(SELECT id
-		FROM ApuestaTipo1
-		WHERE (SELECT ID
-			FROM Partidos
-			WHERE isAbierto = 1) = idPartido
+		(SELECT I.id
+		FROM inserted as I
+		inner join Partidos as P
+		on I.idPartido = P.id
+		WHERE P.isAbierto = 0	//si existen inserciones que estén relacionadas con partidos con el cupo cerrado
 		)
 	BEGIN
 		RAISERROR('EL PERIODO DE APUESTAS ESTA CERRADO',16,1)
@@ -56,3 +56,35 @@ go
 
 SELECT dbo.GananciaDeUnUsuarioConUnaApuesta(4,3) as prueba
 
+
+------------------------------
+/*Trigger para controlar el saldo negativo. Que no puedas apostar más de lo que tienes.*/
+GO
+BEGIN TRAN
+GO
+ALTER
+--CREATE
+TRIGGER noApuestesLoQueNoTienesTipo1
+ON APUESTATIPO1
+AFTER INSERT, UPDATE AS
+BEGIN
+	IF EXISTS
+		(SELECT id
+		FROM inserted as I
+		inner join Usuarios as U
+		on I.correoUsuario = U.correo
+		WHERE saldoActual < I.dinero
+		)
+	BEGIN
+		RAISERROR('No puedes apostar más de lo que tienes',16,1)
+		ROLLBACK
+	END
+
+END
+
+go
+
+/*De momento he hecho solo el de apuestas tipo 1 porque como son los tres iguales, 
+primero quiero que comprobemos que ese está bien. 
+Si lo está, ya copipasteamos los demás, que sería simplemente cambiar la tabla sobre la que 
+operaría el trigger */
