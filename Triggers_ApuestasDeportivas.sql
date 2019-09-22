b33d0b0ea189f5bed654403d1a956a0c7083fc40
AV <angyvazquezdel97@gmail.com>
2019-09-22 18:43:20 +0200

    Funcion escalar para saber cuanto ganaria un usuario con una apuesta si la ganase

--- Triggers_ApuestasDeportivas.sql
+++ Triggers_ApuestasDeportivas.sql
@@ -34,3 +34,25 @@ go
 /*De momento he hecho solo el de apuestas tipo 1 porque como son los tres iguales, primero quiero que comprobemos que
 ese está bien. Si lo está, ya copipasteamos los demás, que sería simplemente cambiar la tabla sobre la que 
 operaría el trigger */
+
+
+----------------------------
+
+/*Funcion escalar para consultar lo que gana un usuario con una apuesta en caso de que acierte
+Se calcula tal que así:
+	ganancia = cantidadApostada x cuota
+*/
+go
+ALTER
+--CREATE 
+FUNCTION 
+GananciaDeUnUsuarioConUnaApuesta (@cuota int, @cantidadApostada int)
+	RETURNS INT AS
+	BEGIN 
+	RETURN (@CUOTA * @CANTIDADAPOSTADA)
+	END
+	
+go
+
+SELECT dbo.GananciaDeUnUsuarioConUnaApuesta(4,3) as prueba
+
